85ee5b39824492ca6bcde1ef07cf0a493b9776f1
Quoc Khanh <nqkhanh.1110@gmail.com>
2020-07-30 11:06:04 +0700

    Init Database

---
+++ app/src/main/java/com/sunasterisk/getitdone/data/source/local/database/AppDatabase.kt
@@ -0,0 +1,35 @@
+package com.sunasterisk.getitdone.data.source.local.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.sunasterisk.getitdone.utils.Database
+
+class AppDatabase private constructor(context: Context, dbName: String, version: Int) :
+    SQLiteOpenHelper(context, dbName, null, version) {
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.run {
+            execSQL(Database.SQL_CREATE_TABLE_LIST)
+            execSQL(Database.SQL_CREATE_TABLE_TASK)
+        }
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.run {
+            execSQL(Database.SQL_DROP_TABLE_LIST)
+            execSQL(Database.SQL_DROP_TABLE_TASK)
+        }
+
+        onCreate(db)
+    }
+
+    companion object {
+        private var instance: AppDatabase? = null
+
+        fun getInstance(context: Context) : AppDatabase =
+            instance ?: AppDatabase(context, Database.DATABASE_NAME, Database.DATABASE_VERSION).also {
+                instance = it
+            }
+    }
+}
